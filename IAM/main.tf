326727673227469a44a9c7a28c4a0137358cb831
Serghei Nacai <98859131+Sergheiss@users.noreply.github.com>
2022-08-06 11:11:03 -0500

    added iam user

--- IAM/main.tf
+++ IAM/main.tf
@@ -1,3 +1,3 @@
-recources "aws_iam_user" "terraform-cloud-test-user" {
-    name = "terraform-cloud-test-user"
+resource "aws_iam_user" "terraform-cloud-test-user" {
+  name = "terraform-cloud-test-user"
 }
